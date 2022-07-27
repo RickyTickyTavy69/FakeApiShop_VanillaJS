fda2b747911c4ee992354544f44059622a675eaa
artem <bakirovartem69@gmail.com>
2022-07-27 22:44:20 +0200

    added menu burger

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,4 @@
-/*const backpack = document.querySelector(".LinkNumber1");
-
-backpack.addEventListener("click", (event) => {
-  console.log("click");
-  window.location.href =
-    "https://www.amazon.com/Fjallraven-Kanken-Laptop-Summer-Landscape/dp/B07HPDF1N2/ref=as_li_ss_tl?_encoding=UTF8&pd_rd_i=B07HPCLX39&pd_rd_r=645b8bce-15e4-498c-b663-8c5968a0b1af&pd_rd_w=9U72T&pd_rd_wg=FOm3S&pf_rd_p=588939de-d3f8-42f1-a3d8-d556eae5797d&pf_rd_r=JY6T0VZKSKK13EXAE1WJ&refRID=JY6T0VZKSKK13EXAE1WJ&th=1&linkCode=sl1&tag=thedsgnshop-20&linkId=e46380d11d72bdbaa7800c18512d508f&language=en_US";
-});*/
+/*****************************links ohne a tags xd */
 
 const backpackSection = document.querySelector(".backpackSection");
 
@@ -164,4 +158,16 @@ bottleSection.addEventListener("click", (event) => {
         break;
     }
   }
+});
+
+/*******************ja is bissl zu lange aber dafür ohne die a Tags */
+
+/******************************************burger menu */
+
+let cheeseburgerButton = document.querySelector(".button_menu");
+let menu = document.querySelector(".menu_menu");
+
+cheeseburgerButton.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  cheeseburgerButton.classList.toggle("active");
 });
