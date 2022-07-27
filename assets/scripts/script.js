79cec6c578f310baa07e4944a08e0d50bd52a1bd
artem <bakirovartem69@gmail.com>
2022-07-27 20:40:53 +0200

    geht eh nicht

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+const backpack = document.querySelector(".backpackLink");
+
+backpack.addEventListener("click", (event) => {
+  console.log("click");
+  window.location.href =
+    "https://www.amazon.com/Fjallraven-Kanken-Laptop-Summer-Landscape/dp/B07HPDF1N2/ref=as_li_ss_tl?_encoding=UTF8&pd_rd_i=B07HPCLX39&pd_rd_r=645b8bce-15e4-498c-b663-8c5968a0b1af&pd_rd_w=9U72T&pd_rd_wg=FOm3S&pf_rd_p=588939de-d3f8-42f1-a3d8-d556eae5797d&pf_rd_r=JY6T0VZKSKK13EXAE1WJ&refRID=JY6T0VZKSKK13EXAE1WJ&th=1&linkCode=sl1&tag=thedsgnshop-20&linkId=e46380d11d72bdbaa7800c18512d508f&language=en_US";
+});
