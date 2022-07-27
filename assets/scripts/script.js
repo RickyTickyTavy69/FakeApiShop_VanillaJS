3edf4d95766e8042eef779427ee530fd9ee30b8e
artem <bakirovartem69@gmail.com>
2022-07-27 21:36:26 +0200

    almost done

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -39,3 +39,71 @@ backpackSection.addEventListener("click", (event) => {
     }
   }
 });
+
+const bookSection = document.querySelector(".bookSection");
+
+bookSection.addEventListener("click", (event) => {
+  if (event.target.classList.contains("Link")) {
+    const linkNumber = String(event.target.classList)[
+      String(event.target.classList).length - 1
+    ];
+    switch (linkNumber) {
+      case "1":
+        window.location.href = "https://amzn.to/2YQLnCV";
+        break;
+      case "2":
+        window.location.href = "https://amzn.to/2I3lbQ7";
+        break;
+      case "3":
+        window.location.href = "https://amzn.to/2IifSvA";
+        break;
+      case "4":
+        window.location.href = "https://amzn.to/2G84CiT";
+        break;
+      case "5":
+        window.location.href = "https://amzn.to/2YWPz3N";
+        break;
+      case "6":
+        window.location.href = "https://amzn.to/2KhwTsz";
+        break;
+      case "7":
+        window.location.href =
+          "https://www.bookdepository.com/From-Japan/9780957081659?a_aid=1991";
+        break;
+    }
+  }
+});
+
+const lampSection = document.querySelector(".lampSection");
+
+lampSection.addEventListener("click", (event) => {
+  if (event.target.classList.contains("Link")) {
+    const linkNumber = String(event.target.classList)[
+      String(event.target.classList).length - 1
+    ];
+    switch (linkNumber) {
+      case "1":
+        window.location.href = "http://www.grupaproducts.com/arigato/";
+        break;
+      case "2":
+        window.location.href = "https://amzn.to/2CZiPxO";
+        break;
+      case "3":
+        window.location.href =
+          "https://heyshop.es/collections/home/products/h-hey";
+        break;
+      case "4":
+        window.location.href = "https://amzn.to/2UGLHEQ";
+        break;
+      case "5":
+        window.location.href = "https://amzn.to/2UFjEWp";
+        break;
+      case "6":
+        window.location.href = "https://amzn.to/2OTHEQg";
+        break;
+      case "7":
+        window.location.href = "https://amzn.to/2UFHfGt";
+        break;
+    }
+  }
+});
